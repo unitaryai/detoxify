703539e8904225caf82e1cb9377603ef5c4edcb3
Laura <laura.hanu10@gmail.com>
2020-09-29 13:34:02 +0000

    update project name

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='project',
-    version='0.0.0',
-    description='Describe Your Cool Project',
-    author='',
-    author_email='',
+    name="src",
+    version="0.0.0",
+    description="Describe Your Cool Project",
+    author="",
+    author_email="",
     # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
-    url='https://github.com/PyTorchLightning/pytorch-lightning-conference-seed',
-    install_requires=['pytorch-lightning'],
+    url="https://github.com/PyTorchLightning/pytorch-lightning-conference-seed",
+    install_requires=["pytorch-lightning"],
     packages=find_packages(),
 )
-
