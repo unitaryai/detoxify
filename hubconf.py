ea3490732f5f0d69038aa4486160be7be5db4c56
Laura <laura.hanu10@gmail.com>
2020-11-05 17:36:53 +0000

    update paths in hubconf

--- hubconf.py
+++ hubconf.py
@@ -1,32 +1,19 @@
-dependencies = ['torch','transformers']
-from train import get_model
+dependencies = ["torch", "transformers"]
 
-MODEL_DEFS = {
-            'original': {'model_type': "bert-base-uncased",
-                           'model_name': "BertForSequenceClassification",
-                           'num_classes': 6},
-            'bias': {'model_type': "roberta-base",
-                     'model_name': "RobertaForSequenceClassification",
-                     'num_classes': 16},
-            'multilingual': {'model_type': "xlm-roberta-base",
-                             'model_name': "XLMRobertaForSequenceClassification",
-                             'num_classes': 1},
-            }
 
 def toxic_bert():
-    model = get_model(**MODEL_DEFS['original'])
-    checkpoint = 'https://github.com/laurahanu/detoxify/releases/download/v0.1-alpha/toxic_original.ckpt'
-    model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=False))
-    return model
+    checkpoint = "https://github.com/laurahanu/detoxify/releases/download/v0.1-alpha/toxic_original-4e693588.ckpt"
+    loaded = torch.hub.load_state_dict_from_url(checkpoint, progress=False)
+    return loaded
+
 
 def toxic_roberta_bias():
-    model = get_model(**MODEL_DEFS['bias'])
-    checkpoint = 'https://github.com/laurahanu/detoxify/releases/download/v0.1-alpha/toxic_bias.ckpt'
-    model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=False))
-    return model
+    checkpoint = "https://github.com/laurahanu/detoxify/releases/download/v0.1-alpha/toxic_bias-4e693588.ckpt"
+    loaded = torch.hub.load_state_dict_from_url(checkpoint, progress=False)
+    return loaded
+
 
 def toxic_xlmr_multilingual():
-    model = get_model(**MODEL_DEFS['multilingual'])
-    checkpoint = 'https://github.com/laurahanu/detoxify/releases/download/v0.1-alpha/toxic_multilingual.ckpt'
-    model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=False))
-    return model
+    checkpoint = "https://github.com/laurahanu/detoxify/releases/download/v0.1-alpha/toxic_multilingual-bbddc277.ckpt"
+    loaded = torch.hub.load_state_dict_from_url(checkpoint, progress=False)
+    return loaded
