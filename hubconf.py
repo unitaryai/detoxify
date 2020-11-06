6caf03d505a895a477dff134c5c4ee51e2f3b25e
Laura <laura.hanu10@gmail.com>
2020-11-06 15:27:15 +0000

    update hubconf to only import class names

--- hubconf.py
+++ hubconf.py
@@ -1,19 +1,7 @@
 dependencies = ["torch", "transformers"]
 
+from detoxify import toxic_bert
 
-def toxic_bert():
-    checkpoint = "https://github.com/laurahanu/detoxify/releases/download/v0.1-alpha/toxic_original-4e693588.ckpt"
-    loaded = torch.hub.load_state_dict_from_url(checkpoint, progress=False)
-    return loaded
+from detoxify import unbiased_toxic_roberta
 
-
-def toxic_roberta_bias():
-    checkpoint = "https://github.com/laurahanu/detoxify/releases/download/v0.1-alpha/toxic_bias-4e693588.ckpt"
-    loaded = torch.hub.load_state_dict_from_url(checkpoint, progress=False)
-    return loaded
-
-
-def toxic_xlmr_multilingual():
-    checkpoint = "https://github.com/laurahanu/detoxify/releases/download/v0.1-alpha/toxic_multilingual-bbddc277.ckpt"
-    loaded = torch.hub.load_state_dict_from_url(checkpoint, progress=False)
-    return loaded
+from detoxify import multilingual_toxic_xlm_r
