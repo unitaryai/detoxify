630c4ab21f943b7f99b279567819556f5f924e5a
Laura <laura.hanu10@gmail.com>
2020-11-09 14:42:49 +0000

    update name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="detoxify-unitary",
+    name="detoxify",
     version="0.0.1",
     description="A python library for detecting toxic comments",
     long_description=long_description,
@@ -18,11 +18,11 @@ setup(
         "transformers",
         "torch",
     ],
-    packages=find_packages(exclude=("tests","src")),
+    packages=find_packages(exclude=("tests", "src")),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
