a56390314c4ae944158bfc1b8409cddfbd9bc1f1
Laura <laura.hanu10@gmail.com>
2020-12-15 21:11:29 +0000

    added lightweight model options to detoxify and hubconf

--- hubconf.py
+++ hubconf.py
@@ -5,3 +5,7 @@ from detoxify import toxic_bert
 from detoxify import unbiased_toxic_roberta
 
 from detoxify import multilingual_toxic_xlm_r
+
+from detoxify import toxic_albert
+
+from detoxify import unbiased_albert
