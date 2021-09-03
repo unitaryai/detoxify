08cc3b9dd050f05662bf1626c52d40a216b131ee
Laura Hanu <32672979+laurahanu@users.noreply.github.com>
2021-09-03 18:15:37 +0100

    delete models after loading

--- tests/test_torch_hub.py
+++ tests/test_torch_hub.py
@@ -1,21 +1,20 @@
 import torch
+import gc
 
 def test_torch_hub_models():
     result = torch.hub.list("unitaryai/detoxify")
-    #assert any(result)
-    assert True
 
 def test_torch_hub_bert():
     model = torch.hub.load('unitaryai/detoxify', 'toxic_bert')
-    #assert ...
-    assert True
+    del model
+    gc.collect()
 
 def test_torch_hub_roberta():
     model = torch.hub.load('unitaryai/detoxify', 'unbiased_toxic_roberta')
-    #assert ...
-    assert True
+    del model
+    gc.collect()
 
 def test_torch_hub_multilingual():
     model = torch.hub.load('unitaryai/detoxify', 'multilingual_toxic_xlm_r')
-    #assert ...
-    assert True
+    del model
+    gc.collect()
