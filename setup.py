e0415cb5f06db88c34ac2b7182faab3cb3651919
Laura Hanu <laura.hanu10@gmail.com>
2021-09-03 22:56:10 +0530

    update to new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="detoxify",
-    version="0.2.2",
+    version="0.3.0",
     description="A python library for detecting toxic comments",
     long_description=long_description,
     long_description_content_type="text/markdown",
