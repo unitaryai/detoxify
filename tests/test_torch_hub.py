02c7cb0b9f6625e02544754718e671254c3a99b6
Laura Hanu <laura.hanu10@gmail.com>
2021-10-26 22:44:28 +0530

    add skip_validation arg

--- tests/test_torch_hub.py
+++ tests/test_torch_hub.py
@@ -5,16 +5,16 @@ def test_torch_hub_models():
     result = torch.hub.list("unitaryai/detoxify")
 
 def test_torch_hub_bert():
-    model = torch.hub.load('unitaryai/detoxify', 'toxic_bert')
+    model = torch.hub.load('unitaryai/detoxify', 'toxic_bert', skip_validation=True)
     del model
     gc.collect()
 
 def test_torch_hub_roberta():
-    model = torch.hub.load('unitaryai/detoxify', 'unbiased_toxic_roberta')
+    model = torch.hub.load('unitaryai/detoxify', 'unbiased_toxic_roberta', skip_validation=True)
     del model
     gc.collect()
 
 def test_torch_hub_multilingual():
-    model = torch.hub.load('unitaryai/detoxify', 'multilingual_toxic_xlm_r')
+    model = torch.hub.load('unitaryai/detoxify', 'multilingual_toxic_xlm_r', skip_validation=True)
     del model
     gc.collect()
