754c1196c2b539e07f8e08163d1f4745cd88b085
Laura Hanu <32672979+laurahanu@users.noreply.github.com>
2023-10-24 00:34:21 +0100

    pass config class directly when loading models

--- detoxify/detoxify.py
+++ detoxify/detoxify.py
@@ -1,5 +1,6 @@
 import torch
 import transformers
+from transformers import AutoConfig
 
 DOWNLOAD_URL = "https://github.com/unitaryai/detoxify/releases/download/"
 MODEL_URLS = {
@@ -17,9 +18,11 @@ def get_model_and_tokenizer(
     model_type, model_name, tokenizer_name, num_classes, state_dict, huggingface_config_path=None
 ):
     model_class = getattr(transformers, model_name)
+    if not huggingface_config_path:
+        config = AutoConfig.from_pretrained(model_type, num_labels=num_classes)
     model = model_class.from_pretrained(
         pretrained_model_name_or_path=None,
-        config=huggingface_config_path or model_type,
+        config=huggingface_config_path or config,
         num_labels=num_classes,
         state_dict=state_dict,
         local_files_only=huggingface_config_path is not None,
