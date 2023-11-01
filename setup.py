2db4ec30ffd4a950921e9921c9d471dacb00e23c
Vasilije <8619304+Vasilije1990@users.noreply.github.com>
2023-11-01 14:40:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     author_email="laura@unitary.ai",
     url="https://github.com/unitaryai/detoxify",
     install_requires=[
-        "transformers == 4.30.0",
+        "transformers == 4.33.4",
         "torch >= 1.7.0",
         "sentencepiece >= 0.1.94",
     ],
